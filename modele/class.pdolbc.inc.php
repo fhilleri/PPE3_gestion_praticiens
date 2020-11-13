cb49ccdde031bd166bf7efcebd6f953d9c6a2492
duvalsoraya <duvals@saintemarie-cholet.eu>
2020-11-13 11:40:09 +0100

    portefeuille responsable

--- modele/class.pdolbc.inc.php
+++ modele/class.pdolbc.inc.php
@@ -54,7 +54,7 @@ class Pdolbc
 		return $lesLignes;
 	}
 
-	/* Affiche Portefeuille lié au visiteur*/	
+	/* Affiche le portefeuille lié au visiteur*/	
 
 	public function getPorteFeuilleVis() {
 		$req = "select * from portefeuille";
@@ -63,5 +63,12 @@ class Pdolbc
 		return $lesLignes;
 	}
 
+	/* Supprimer les praticiens */
+	public function getSupprimerPraticien() {
+		$req = "Delete from portefeuille where idpraticien=????";
+		$res = Pdolbc::$monPdo->query($req);
+		$lesLignes = $res->fetchAll();
+		return $lesLignes;
+	}
 }
 ?>
