8919d850d2109c1468c1d43d1139be929aea554a
jadeaua <72965310+jadeaua@users.noreply.github.com>
2020-11-27 08:40:16 +0100

    cssco

---
+++ vues/v_connexion.php
@@ -0,0 +1,21 @@
+<nav id="connexion">
+<main>
+    <div id="loginbox">
+        <img src="/images/avatar.png" id="avatar">
+    <form method="POST" action="index.php?uc=praticiens&ucp=connexion&action=validerConnexion">
+        <table>
+            <tr>
+                <td><label for="login">Login :</label></td>
+                <td><input type="text" name="login"></td>
+            </tr>
+            <tr>
+                <td><label for="mdp">Mot de passe :</label></td>
+                <td><input type="password" name="mdp"></td>
+            </tr>
+        </table>
+
+        <input type="submit" value="Connexion">
+    </form>
+</div>
+</main>
+</nav>
